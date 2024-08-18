1f3d93b0b14097d0f3c27bf90fd5159cb80d0d7e
GhulMasters <sebastdroid@gmail.com>
2024-08-18 16:41:41 +0000

    Mundo guardado 2024-08-18

--- server.py
+++ server.py
@@ -60,4 +60,5 @@ flnm=download_latest_release()
 if flnm.split(".")[-1] == "msp":
 	os.system(f"chmod +x {flnm} && ./{flnm}")
 else:
-    os.system(f"python3 {flnm}")RE
+    os.system(f"python3 {flnm}")
+	
